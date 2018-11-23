6df06dd89f34cfd7256b92b20fcefaa189454f69
gogoend <gogoend@qq.com>
2018-11-23 12:59:55 +0800

    本地调试

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ App({
     doNotAutoShowLogin:false
   },
   globalConfig:{
-    baseDomain:'https://blog.me4vr.com',
-    blogURL:'https://blog.me4vr.com/wp-json/wp/v2'
+    o_baseDomain:'https://blog.me4vr.com',
+    o_blogURL:'https://blog.me4vr.com/wp-json/wp/v2',
+    baseDomain: 'http://localhost',
+    blogURL: 'http://localhost/wp-json/wp/v2'
   }
 });
